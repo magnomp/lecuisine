8d0cc8f6ecd86ac9b1a52ee3524aa46b5ddaaa6a
Magno Machado <magnomp@gmail.com>
2022-04-01 05:36:35 -0300

    Changed JWT parameters
    Made some hardcoded constants, and secret from injection

--- server/src/main/kotlin/lecuisine/Application.kt
+++ server/src/main/kotlin/lecuisine/Application.kt
@@ -5,11 +5,12 @@ import io.ktor.server.cio.*
 import lecuisine.plugins.*
 
 fun main() {
+    val leCuisine: LeCuisine = DaggerLeCuisine.create()
     embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
         configureAdministration()
         configureRouting()
         configureSerialization()
-        configureSecurity()
+        configureSecurity(leCuisine.jwtSecret())
         configureHTTP()
     }.start(wait = true)
 }
--- server/src/main/kotlin/lecuisine/plugins/Security.kt
+++ server/src/main/kotlin/lecuisine/plugins/Security.kt
@@ -7,20 +7,19 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
-fun Application.configureSecurity() {
+private const val jwtAudience = "LeCuisine"
+
+fun Application.configureSecurity(jwtSecret: String) {
     
     authentication {
             jwt {
-                val jwtAudience = environment.config.property("jwt.audience").getString()
-                realm = environment.config.property("jwt.realm").getString()
+                realm = "Access to LeCuisine app"
                 verifier(
                     JWT
                         .require(Algorithm.HMAC256("secret"))
                         .withAudience(jwtAudience)
-                        .withIssuer(environment.config.property("jwt.domain").getString())
+                        .withIssuer("LeCuisine")
                         .build()
                 )
                 validate { credential ->
